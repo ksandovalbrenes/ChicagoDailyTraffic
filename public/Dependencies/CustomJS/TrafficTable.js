285efdedb3fec04dbbb5ef6333bf2c446ec7aae9
Kenneth Sandoval <kennethsandovalb@gmail.com>
2019-01-18 09:25:36 -0600

    get coordinates values

--- public/Dependencies/CustomJS/TrafficTable.js
+++ public/Dependencies/CustomJS/TrafficTable.js
@@ -5,7 +5,7 @@ $(document).ready( function () {
             dataSrc: ''
         },
         columns: [
-            {data:"cc", "defaultContent": ""},
+            {data:"title"},
             {data: null, render: function (data, type, row){
                 var date = new Date(data.date);
                 return data.day + " " + date.getFullYear() + "/" + date.getMonth() + "/" + date.getDay();
@@ -13,12 +13,18 @@ $(document).ready( function () {
             {data: null, render: function(data, type, row){
                 return data.location_address + ", " + data.location_city + ", " + data.location_state;
             }},
-            {data:"park"},
-            {data:"park_address"},
+            {data: null, render: function (data, type, row){
+                return data.park + " - " + data.park_address;
+            }},
             {data:"park_phone"},
             {data:"rating"},
-            {data:"title"},
-            {data:"underwriter","defaultContent": ""}
+            {data: null, render: function (data, type, row) {
+                if (data.location != null){
+                    return 'x:' + data.location.coordinates[0]  + ' y' + data.location.coordinates[1];
+                } else {
+                    return '';
+                }
+            }}
         ],
         responsive: true
     } );
